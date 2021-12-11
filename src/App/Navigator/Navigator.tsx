98cdd19d79d1268beee2a19697ab6ba02247d0e6
DRob1260 <drew.robert26@me.com>
2021-12-11 12:24:40 -0600

    remove unused variable

--- src/App/Navigator/Navigator.tsx
+++ src/App/Navigator/Navigator.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import './Navigator.scss';
 import { AppBar, Button, Toolbar } from '@mui/material';
-import { useAuth0 } from '@auth0/auth0-react';
 import { AuthButton } from './AuthButton/AuthButton';
 
 export const Navigator: React.FunctionComponent = () => {
-    const { loginWithRedirect } = useAuth0();
-
     return (
         <div className={'Navigator'}>
             <AppBar>
